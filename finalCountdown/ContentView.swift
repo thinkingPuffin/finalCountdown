a210b645cd6031fc6bd59b0e627cd33810d443a9
Felix MIL <felixmil@outlook.com>
2022-07-27 20:38:39 +0200

    Initial Commit

---
+++ finalCountdown/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  finalCountdown
+//
+//  Created by Félix MIL on 27/07/2022.
+//  Copyright © 2022 Félix MIL. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
